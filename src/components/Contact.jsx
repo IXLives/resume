2f19786684386f7bab846e38b3c8182f7e17885d
IXLives <bryant.ian87@gmail.com>
2023-09-20 16:52:33 -0400

    email form update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,9 +37,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Ian Bryant",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: VITE_DEV_EMAIL,
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
